086bc6e86696d6b012e4c7d56523b41d7cfdc1a5
ASHISH <dr.ashish.g77@gmail.com>
2021-11-24 09:40:28 +0530

    Delete secret.js

--- src/utils/secret.js
+++
@@ -1,7 +0,0 @@
-const secrets = {
-  client_id: 'a9cf283cd4ed4197a337970497a3e459',
-  client_secret: '0ae16c40cba545529ad67ef3394e3db9',
-  redirect_uri: 'http://localhost:3000/',
-  base_url: 'https://api.spotify.com/v1',
-}
-export default secrets
